85bffb2b6905d7688c8bd6f2c49d023a4b49b7e5
Yuri Bohatyuk <yurii.bohatiuk@namely.com>
2019-11-19 12:16:49 +0200

    Initial commit

---
+++ src/TestSystem.Server/Repositories/AuthorsRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TestSystem.Common;
+
+namespace WebApiServer.Repositories
+{
+    public class AuthorsRepository:IAuthorsRepository
+    {
+        public IEnumerable<Author> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Update(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
