054ada641509982c199f980b2842c2974c132ec9
vova-kuharchyk <50716374+vova-kuharchyk@users.noreply.github.com>
2019-11-19 21:28:02 +0200

    Fix this bulshit (#6)

---
+++ src/TestSystem.Common/TestExam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TestSystem.Designer.Library;
+
+namespace TestSystem.Common
+{
+    [Serializable]
+    public class TestExam
+    {
+        public  Test BodyTest { get; set; } = new Test();
+        public int Result { get; set; }
+    }
+}
