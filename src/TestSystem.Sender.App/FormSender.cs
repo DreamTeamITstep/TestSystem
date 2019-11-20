eae9862fb5dab0f6f74cbe7ecbd9e381cfea5dbb
Vova <vovafalkon@gmail.com>
2019-11-20 16:15:25 +0200

    Fix Referetce

--- src/TestSystem.Sender.App/FormSender.cs
+++ src/TestSystem.Sender.App/FormSender.cs
@@ -1,14 +1,7 @@
 ﻿using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 using TestSystem.Common;
